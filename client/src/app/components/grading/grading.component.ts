06b691579bbe5028649d8c3fdc40525bc61f200b
YoussefTakali <yousseftakali159@gmail.com>
2025-04-25 11:24:55 +0100

    added grade section

--- client/src/app/components/grading/grading.component.ts
+++ client/src/app/components/grading/grading.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
   selector: 'app-grading',
   templateUrl: './grading.component.html',
   styleUrls: ['./grading.component.css']
 })
-export class GradingComponent {
+export class GradingComponent implements OnInit,OnDestroy {
+  sidebarVisible: boolean = false;
+  private sidebarSub!: Subscription;
 
+  constructor(private sidebarService: SidebarService) {}
+
+  ngOnInit(): void {
+    this.sidebarSub = this.sidebarService.sidebarVisible$.subscribe((visible:boolean) => {
+      this.sidebarVisible = visible;
+      console.log('Sidebar visibility in SubmissionsComponent:', this.sidebarVisible);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.sidebarSub) {
+      this.sidebarSub.unsubscribe();
+    }
+  }
+
+  submissions = [
+    {
+      title: 'Database Design',
+      description: 'Submitted optimized schema with documentation',
+      author: 'Jamie Smith',
+      time: '3 days ago',
+      grade: 'A',
+      aiSuggestion:55
+    },
+    {
+      title: 'Database Design',
+      description: 'Submitted optimized schema with documentation',
+      author: 'Jamie Smith',
+      time: '3 days ago',
+      grade: 'B',
+      aiSuggestion:null
+
+    },
+    {
+      title: 'Database Design',
+      description: 'Submitted optimized schema with documentation',
+      author: 'Jamie Smith',
+      time: '3 days ago',
+      grade: 'C',
+      aiSuggestion:18
+    },
+    {
+      title: 'Database Design',
+      description: 'Submitted optimized schema with documentation',
+      author: 'Jamie Smith',
+      time: '3 days ago',
+      grade: 'D',
+      aiSuggestion:40
+
+    },
+    {
+      title: 'Database Design',
+      description: 'Submitted optimized schema with documentation',
+      author: 'Jamie Smith',
+      time: '3 days ago',
+      grade: 'A',
+      aiSuggestion:90
+    }
+  ];
 }
