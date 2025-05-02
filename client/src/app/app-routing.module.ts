f1aa4c28e6ee3f5cfc78cbe496a347122cb21396
aichabnr <aichabenromdhane06@gmail.com>
2025-05-02 10:17:25 +0100

    add keyclaok

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from './keycloak/app.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component:MainLayoutComponent,
+    canActivate: [AuthGuard], // ✅ protection par Keycloak
+    children:[
+      {path:'', redirectTo:'dashboard',pathMatch:'full'},
+      { path:'dashboard',component:DashboardComponent},
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ client/src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AssignmentcardComponent } from './components/assignmentcard/assignmentcard.component';
+import { StarcardComponent } from './components/starcard/starcard.component';
+import { LucideAngularModule, BookOpenText, GitCommit, Calendar, GraduationCap } from 'lucide-angular';
+import { ActivityComponent } from './components/activity/activity.component';
+import { SubmissionsComponent } from './components/submissions/submissions.component';
+import { ProgressComponent } from './components/progress/progress.component';
+import { GradingComponent } from './components/grading/grading.component';
+import { initializeKeycloak } from './keycloak/app.init';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainLayoutComponent,
+    NavbarComponent,
+    SidebarComponent,
+    DashboardComponent,
+    AssignmentcardComponent,
+    StarcardComponent,
+    ActivityComponent,
+    SubmissionsComponent,
+    ProgressComponent,
+    GradingComponent,
+  ],
+  imports: [
+    BrowserModule,
+    KeycloakAngularModule,
+    AppRoutingModule,
+
+    LucideAngularModule.pick({ 
+      BookOpenText, 
+      GitCommit, 
+      Calendar, 
+      GraduationCap 
+    })
+  ],
+  providers: [
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializeKeycloak,
+      multi: true,
+      deps: [KeycloakService],
+    }
+  ],
+  
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
