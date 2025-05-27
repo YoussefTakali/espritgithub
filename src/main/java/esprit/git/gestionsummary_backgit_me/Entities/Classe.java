5692f900e77e8c2320d125b4a48fa09bb208bdaf
Ryma Ezzar <rima.ezzar@esprit.tn>
2025-05-27 19:56:49 +0100

    add controller

--- src/main/java/esprit/git/gestionsummary_backgit_me/Entities/Classe.java
+++ src/main/java/esprit/git/gestionsummary_backgit_me/Entities/Classe.java
@@ -1,38 +1,30 @@
 package esprit.git.gestionsummary_backgit_me.Entities;
 
 import jakarta.persistence.*;
-import jakarta.websocket.server.ServerEndpoint;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
+@Table(name = "classe")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class Classe {
-    @ManyToOne
-    Niveau niveau;
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long id;
-    int number;
+    private Long id;
 
-    public int getNumber() {
-        return number;
-    }
+    @NotNull(message = "Le numéro de classe ne peut pas être null")
+    @Min(value = 1, message = "Le numéro de classe doit être supérieur à 0")
+    private int number;
 
-    public void setNumber(int number) {
-        this.number = number;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
+    @ManyToOne
+    @JoinColumn(name = "niveau_id", nullable = false)
+    @NotNull(message = "Le niveau ne peut pas être null")
+    private Niveau niveau;
 }
